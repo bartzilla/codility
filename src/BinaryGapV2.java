d876d256899f9c1d0d20d4f9b3e41b0b998fb8e1
Cipriano Sanchez <ciprianosanchez@gmail.com>
2017-06-20 13:49:54 +0200

    Adding a second version of BinaryGap

---
+++ src/BinaryGapV2.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ *  Correctness: 100%
+ *  Performance: not assessed
+ *  Task score: 100%
+ */
+
+public class BinaryGapV2
+{
+    public static void main(String[] args)
+    {
+        final int number = 255;
+        System.out.println("binary representation: " + Integer.toBinaryString(number));
+        System.out.println(BinaryGapV2.getLongestBinaryGap(number));
+    }
+
+    private static int getLongestBinaryGap(final int number)
+    {
+        final String binaryString = Integer.toBinaryString(number);
+        List<Integer> indexes = new ArrayList<>();
+        List<List<String>> arrayOfElements = new ArrayList<>();
+
+        char[] chars = binaryString.toCharArray();
+        List<String> binaryElements = new ArrayList<>();
+
+        for (char aChar : chars)
+        {
+            binaryElements.add(String.valueOf(aChar));
+        }
+
+        // 1 0 0 0 0 1 0 1 0 0 0
+        for (int i = 0; i < binaryString.length(); i++)
+        {
+            if (binaryString.charAt(i) == '1')
+            {
+                indexes.add(i);
+            }
+        }
+
+        for (int i = 0; i < indexes.size() - 1; i++)
+        {
+            arrayOfElements.add(binaryElements.subList(indexes.get(i), indexes.get(i + 1) + 1));
+        }
+
+        int biggestGap = 0;
+        int gap = 0;
+        for (List<String> arrayOfElement : arrayOfElements)
+        {
+            gap = arrayOfElement.size() - 2;
+
+            if (gap > biggestGap)
+            {
+                biggestGap = gap;
+            }
+        }
+        return biggestGap;
+    }
+}
