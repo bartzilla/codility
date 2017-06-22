d2406f8c322546e1de7d68aba60c16c714a7dd57
Cipriano Sanchez <ciprianosanchez@gmail.com>
2017-06-22 09:01:16 +0200

    Missing element in an array

---
+++ src/PermMissingElem.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+/*
+ *  Correctness: 100%
+ *  Performance: 100%
+ *  Task score:  100%
+ */
+public class PermMissingElem
+{
+    public static void main(String[] args)
+    {
+        final int[] elements = {4,3,1,2};
+
+        System.out.println("The missing number in the array is: " + PermMissingElem.getMissingElement(elements));
+    }
+
+    private static int getMissingElement(final int[] A)
+    {
+        if (A.length == 0)
+        {
+            return 1;
+        }
+
+        Arrays.sort(A);
+
+        for(int i=0; i<A.length; i++)
+        {
+            int sequence = i + 1;
+            if(A[i] != sequence)
+            {
+                return sequence;
+            }
+        }
+
+        return A.length + 1;
+    }
+}
